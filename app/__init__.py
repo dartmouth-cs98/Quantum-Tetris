7ffb47efb0fd584daea14703a6315466cb98df17
xrzhuang <34495807+xrzhuang@users.noreply.github.com>
2019-05-16 11:25:54 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
     )
 
     if test_config is None:
