3b51681c0bac6ed39e1f8ea17a0107069f7a36b4
Trevor <Trevor.T.Glasgow.20@dartmouth.edu>
2019-10-31 18:07:43 -0500

    Fixed endpoint issue and renamed a bunch

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -7,33 +7,33 @@ from app.player import Player
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-    app.config.from_mapping(
+    application = Flask(__name__, instance_relative_config=True)
+    cors = CORS(application, resources={r"/api/*": {"origins": "*"}})
+    application.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+        DATABASE=os.path.join(application.instance_path, 'app.sqlite'),
     )
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        application.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        application.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
-        os.makedirs(app.instance_path)
+        os.makedirs(application.instance_path)
     except OSError:
         pass
 
     # Commented out so that we can run everything without a db for now
-    db.init_app(app)
-    app.register_blueprint(game.bp)
-    app.add_url_rule('/', endpoint='game')
+    db.init_app(application)
+    application.register_blueprint(game.bp)
+    application.add_url_rule('/', endpoint='game')
 
 
     player=Player()
-    @app.route('/api/createPlayer', methods=['POST'])
+    @application.route('/api/createPlayer', methods=['POST'])
     def createPlayer():
         if request.method == 'POST':
             params=request.get_json()
@@ -44,7 +44,7 @@ def create_app(test_config=None):
         print("CreatePlayer")
 
 
-    @app.route('/api/fetchPlayer', methods=['GET'])
+    @application.route('/api/fetchPlayer', methods=['GET'])
     def fetchPlayer():
         if request.method == 'GET':
             username = request.username
@@ -52,9 +52,9 @@ def create_app(test_config=None):
             return response
         print("FetchPlayer")
 
-    @app.route('/api/deletePlayer', methods=['DELETE'])
+    @application.route('/api/deletePlayer', methods=['DELETE'])
     def delete():
 
         return player
 
-    return app
+    return application
