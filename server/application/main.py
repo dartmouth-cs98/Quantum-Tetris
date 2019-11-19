48c52b4a3c3b25ece02fd9ebb71d3acde70d01c3
Trevor <Trevor.T.Glasgow.20@dartmouth.edu>
2019-11-18 18:51:08 -0600

    More Heroku Fixes

--- server/application/main.py
+++ server/application/main.py
@@ -2,6 +2,7 @@ import os
 from application.__init__ import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ['PORT'])
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 else:
     gunicorn_app = app
