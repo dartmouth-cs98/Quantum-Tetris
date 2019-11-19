48fa68ced8fc999760a13a3b0742fb00a03185b9
Trevor <Trevor.T.Glasgow.20@dartmouth.edu>
2019-11-18 21:32:39 -0600

    Changed Port

--- server/application/main.py
+++ server/application/main.py
@@ -3,6 +3,6 @@ from application.__init__ import app
 
 if __name__ == '__main__':
     # port = int(os.environ.get('PORT', 5000))
-    app.run()
+   app.run(host= '0.0.0.0')
 else:
     gunicorn_app = app
