a00f30faa62921cfe19de26995b8de71dafd2bc7
Trevor <Trevor.T.Glasgow.20@dartmouth.edu>
2019-11-18 18:04:23 -0600

    Updated Requirements

--- server/application/main.py
+++ server/application/main.py
@@ -1,6 +1,7 @@
+import os
 from application.__init__ import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=os.environ['PORT'])
 else:
     gunicorn_app = app
