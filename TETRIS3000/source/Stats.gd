f1c70747db10115427865c9f20fab3ddf3545e14
Phil-Hanlon <rafael.brantley@gmail.com>
2020-01-21 17:26:18 -0500

    Final commit

--- TETRIS3000/source/Stats.gd
+++ TETRIS3000/source/Stats.gd
@@ -20,9 +20,15 @@ var combos
 signal level_up(level)
 signal flash_text(text)
 
+
+# Gets called as soon as this scene appears in the scene-tree
+# See https://docs.godotengine.org/en/3.1/getting_started/step_by_step/scripting_continued.html
 func _ready():
 	load_user_data()
 	
+	
+# Loads the player's highscore
+# We could also use this function to store other helpful data about the user
 func load_user_data():
 	var save_game = File.new()
 	if not save_game.file_exists("user://data.save"):
@@ -33,6 +39,7 @@ func load_user_data():
 		$VBC/HighScore.text = str(high_score)
 		save_game.close()
 	
+# Implements starting the new game
 func new_game(start_level):
 	level = start_level - 1
 	goal = 0
@@ -42,7 +49,9 @@ func new_game(start_level):
 	$VBC/Time.text = "0:00:00"
 	combos = -1
 	new_level()
-	
+
+
+# Called whenever the player reaches a new level
 func new_level():
 	level += 1
 	goal += 5 * level
@@ -51,9 +60,14 @@ func new_level():
 	emit_signal("flash_text", "Level\n%d"%level)
 	emit_signal("level_up", level)
 
+
+# Unclear when this actually gets called,
+# but is unlikely to be important for our purposes
 func _on_Clock_timeout():
 	show_time()
 	
+	
+
 func show_time():
 	var time_elapsed = OS.get_system_time_secs() - time
 	var seconds = time_elapsed % 60
@@ -61,10 +75,15 @@ func show_time():
 	var hours = int(time_elapsed/3600)
 	$VBC/Time.text = str(hours) + ":%02d"%minutes + ":%02d"%seconds
 
+
+
 func piece_dropped(ds):
 	score += ds
 	$VBC/Score.text = str(score)
 
+
+# Triggered whenever the player drops a piece to the bottom,
+# but only does anything if the piece fills any lines.
 func piece_locked(lines, t_spin):
 	var ds
 	if lines or t_spin:
@@ -100,11 +119,14 @@ func piece_locked(lines, t_spin):
 	if goal <= 0:
 		new_level()
 	
+	
 func _notification(what):
 	match what:
 		MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
 			save_user_data()
 
+
+# Saves the player's highscore
 func save_user_data():
 	var save_game = File.new()
 	save_game.open_encrypted_with_pass("user://data.save", File.WRITE, password)
